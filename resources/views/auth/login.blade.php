3b8ec499fcd36fa7798287e50967b84bb3ef7e24
Alexis <alexis.contreras01@alumnos.ucn.cl>
2023-10-15 20:59:12 -0300

    Instalando bootstrap

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,22 +2,23 @@
 <html lang="es">
 
 <head>
-    <title>Turjoy | Log-in</title>
+    <title>Turjoy | Iniciar Sesión</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Iniciar Sesión</title>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     @vite(['resources/css/Login.css'])
-    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+
 
 </head>
 
 <body>
 
+
     <div class="container">
-        <h1>Iniciar sesión</h1>
+
         <form method="POST" action="{{ route('Auth') }}" novalidate>
+            <h1>Iniciar sesion</h1>
             @csrf
             <div class="email-container">
                 <input type="email" name="email" id="email" placeholder="Correo electrónico">
@@ -26,18 +27,16 @@
                 @enderror
             </div>
             <div class="password-container">
-                <input type="password" name="password" id="contrasena" placeholder="Contraseña">
 
-                <button type="button" class="show-password-button" onclick="togglePasswordVisibility()">Mostrar contraseña</button>
+                <input type="password" name="password" id="contrasena" placeholder="Contraseña">
                 @error('password')
                     <p>{{ $message }}</p>
                 @enderror
-
-
             </div>
             @if (session('message'))
-                <p>{{ session('message') }}</p>
+            <p>{{ session('message') }}</p>
             @endif
+
             <div class="button-container">
                 <button type="submit" class="login-button">Ingresar</button>
                 <a href="{{ route('Home') }}" class="back-button">Volver</a>
@@ -47,29 +46,9 @@
         </form>
     </div>
 
-    <script>
-        window.addEventListener("load", function () {
-            document.body.classList.add("loaded");
-        });
-    </script>
 
-
-    <script>
-        // Función para alternar la visibilidad de la contraseña
-        function togglePasswordVisibility() {
-            var passwordInput = document.getElementById("contrasena");
-            if (passwordInput.type === "password") {
-                passwordInput.type = "text";
-            } else {
-                passwordInput.type = "password";
-            }
-        }
-    </script>
-
-
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 
 </body>
 </html>
